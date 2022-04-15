63182eeb2a765b77a112a77863099a5740f79074
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-04-15 10:43:06 +0200

    ajout de UsersFixtures et fin des datafixtures

--- src/DataFixtures/NotificationsFixtures.php
+++ src/DataFixtures/NotificationsFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Notifications;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,14 @@ class NotificationsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        for($notif = 0; $notif <=10; $notif++)
+        {
+        $faker = \Faker\Factory::create('fr_FR');
+        $notification = new Notifications();
+        $notification->setStatus($faker->word());
+        $manager->persist($notification);
+        }
+        
         $manager->flush();
     }
 }
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsersFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder
+    ){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
 
+        for($usr = 0; $usr <= 10; $usr++)
+        {
+        $user = new Users();
+        $user->setFirstname($faker->firstName);
+        $user->setLastname($faker->lastName);
+        $user->setUsername($faker->userName);
+        $user->setEmail($faker->email);
+        $user->setPassword(
+            $this->passwordEncoder->HashPassword($user, 'password')
+        );
+        $user->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+        }
         $manager->flush();
     }
 }
