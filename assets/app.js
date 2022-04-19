8229147ed2c5c3c65f55c0ed4815529e08827473
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-04-19 08:59:00 +0200

    fin de la mise en place du bundle reset password

--- assets/app.js
+++ assets/app.js
@@ -10,6 +10,7 @@ import './styles/app.scss';
 import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
 
+
 // start the Stimulus application
 import './bootstrap';
 bsCustomFileInput.init();
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('base.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
