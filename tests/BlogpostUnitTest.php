71ee40d3d29b32f43ad1361b34916695060a06e5
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-04-22 13:58:44 +0200

    mise en page de la page d'accueil

--- tests/BlogpostUnitTest.php
+++ tests/BlogpostUnitTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 
+use App\Entity\User;
 use DateTimeImmutable;
 use App\Entity\Blogpost;
 use PHPUnit\Framework\TestCase;
@@ -15,16 +16,19 @@ class BlogpostUnitTest extends TestCase
     {
         $blogpost = new Blogpost();
         $datetime = new DateTimeImmutable();
+        $user = new User();
 
         $blogpost->setTitre('titre')
             ->setContenu('contenu')
             ->setCreatedAt($datetime)
-            ->setSlug('slug');
+            ->setSlug('slug')
+            ->setUser($user);
 
         $this->assertTrue($blogpost->getTitre() === 'titre');
         $this->assertTrue($blogpost->getContenu() === 'contenu');
         $this->assertTrue($blogpost->getCreatedAt() === $datetime);
         $this->assertTrue($blogpost->getSlug() === 'slug');
+        $this->assertTrue($blogpost->getUser() === $user);
     }
 
     // test que la valeur est attendue est false
@@ -32,16 +36,20 @@ class BlogpostUnitTest extends TestCase
     {
         $blogpost = new Blogpost();
         $datetime = new DateTimeImmutable();
+        $user = new User();
 
         $blogpost->setTitre('titre')
             ->setContenu('contenu')
             ->setCreatedAt($datetime)
-            ->setSlug('slug');
+            ->setSlug('slug')
+            ->setUser($user);
+
 
         $this->assertFalse($blogpost->getTitre() === 'false');
         $this->assertFalse($blogpost->getContenu() === 'false');
         $this->assertFalse($blogpost->getCreatedAt() ===  new DateTimeImmutable());
         $this->assertFalse($blogpost->getSlug() === 'false');
+        $this->assertFalse($blogpost->getUser() === new User());
     }
 
     // test que la valeur est attendue est empty
@@ -53,6 +61,7 @@ class BlogpostUnitTest extends TestCase
         $this->assertEmpty($blogpost->getContenu());
         $this->assertEmpty($blogpost->getCreatedAt());
         $this->assertEmpty($blogpost->getSlug());
+        $this->assertEmpty($blogpost->getUser());
     }
     
     
